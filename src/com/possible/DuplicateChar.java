60af9aaefe449bc6e13004a109de525d4f79570f
Akinfemiwa Abayomi Olayode <olayodepossible@gmail.com>
2020-06-26 14:21:10 +0100

    code refactor

--- src/com/possible/DuplicateChar.java
+++ src/com/possible/DuplicateChar.java
@@ -8,14 +8,11 @@ public class DuplicateChar {
             //Converts given string into character array
             char string[] = string1.toCharArray();
 
-            System.out.println("Duplicate characters: ");
-            //Counts each character present in the string
             for(int i = 0; i <string.length; i++) {
                 count = 1;
                 for(int j = i+1; j <string.length; j++) {
                     if(string[i] == string[j] && string[i] != ' ') {
                         count++;
-                        //Set string[j] to 0 to avoid printing visited character
                         string[j] = '0';
                     }
                 }
